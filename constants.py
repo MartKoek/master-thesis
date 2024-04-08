8c0edc7f2268cd0be1920df5e3c2fb195ea4cfca
MartKoek <59614066+MartKoek@users.noreply.github.com>
2024-04-08 10:55:33 +0200

    New paths

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 SBERT_MODELS = ["sbert_bert-base-nli-mean-tokens", "sbert_all-MiniLM-L12-v2", "sbert_all-mpnet-base-v2"]
 
-DIR_TRAIN = 'data/training/'
-DIR_DEV = 'data/development/'
-DIR_TEST = 'data/test/'
+DIR_TRAIN = 'C:/Users/mjkoe/Thesis/data/training/'
+DIR_DEV = 'C:/Users/mjkoe/Thesis/data/development/'
+DIR_TEST = 'C:/Users/mjkoe/Thesis/data/test/'
 
 FUNCTIONALS = ["mean", "var", "std", "median", "quantile", "max"]
