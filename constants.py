7093293631ea93770c8c29dea009f297adffe8c5
MartKoek <59614066+MartKoek@users.noreply.github.com>
2024-07-04 10:19:58 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 SBERT_MODELS = ["sbert_bert-base-nli-mean-tokens", "sbert_all-MiniLM-L12-v2", "sbert_all-mpnet-base-v2"]
 
-DIR_TRAIN = 'C:/Users/mjkoe/Thesis/data/training/'
-DIR_DEV = 'C:/Users/mjkoe/Thesis/data/development/'
-DIR_TEST = 'C:/Users/mjkoe/Thesis/data/test/'
+DIR_TRAIN = ''  # directory of the training set from DAIC-WOZ
+DIR_DEV = ''  # directory of the development set from DAIC-WOZ
+DIR_TEST =  ''  # directory of the test set from DAIC-WOZ
 
 FUNCTIONALS = ["mean", "var", "std", "median", "quantile", "max"]
